7684ec6f987ef28c963058986b30ec96471e91d6
semerded <sem.van.broekhoven@gmail.com>
2023-10-09 08:28:34 +0200

    aids

--- volkomenRoodLicht.py
+++ volkomenRoodLicht.py
@@ -1,6 +1,7 @@
-from gpiozero import LED, LightSensor
-from time import sleep
+from gpiozero import LED, LightSensor # import de componenten uit de library die je nodig hebt
+from time import sleep # om delay's toe te voegen
 
+# duid de pinnen aan van de verschillende componenten
 AUTOROOD = LED(10)
 AUTOGEEL = LED(9)
 AUTOGROEN = LED(11)
@@ -11,48 +12,41 @@ FIETSGROEN = LED(26)
 
 LDR = LightSensor(21)
 
-counter = 0
-
-class stoplicht:
-    def __init__(self, lichtRood, lichtGeel, lichtGroen) -> None:
+class stoplicht: # maak een class aan met de verschillende standen van het stoplicht
+    def __init__(self, lichtRood, lichtGeel, lichtGroen) -> None: # maak de class aan en voeg de verschillende lichten toe
         self.__lichtRood = lichtRood
         self.__lichtGeel = lichtGeel
         self.__lichtGroen = lichtGroen
         
         
-    def Rood(self):
+    def Rood(self): # lichtstand rood
         self.__lichtRood.on()
         self.__lichtGeel.off()
         self.__lichtGroen.off()
         
     
-    def Geel(self):
+    def Geel(self): # lichtstand geel
         self.__lichtRood.off()
         self.__lichtGeel.on()
         self.__lichtGroen.off()
 
         
-    def Groen(self): 
+    def Groen(self): # lichtstand groen
         self.__lichtRood.off()
         self.__lichtGeel.off()
         self.__lichtGroen.on()       
     
-fietsLicht = stoplicht(FIETSROOD, FIETSGEEL, FIETSGROEN)
-autoLicht = stoplicht(AUTOROOD, AUTOGEEL, AUTOGROEN)
+fietsLicht = stoplicht(FIETSROOD, FIETSGEEL, FIETSGROEN) # voeg de standen van de class aan de fietslichten toe
+autoLicht = stoplicht(AUTOROOD, AUTOGEEL, AUTOGROEN) # voeg de standen van de class aan de autolichten toe
 
+tijdGeel = 1 # de tijd dat het geel is
+tijdGroen = 5 # de tijd dat het groen/rood is
 
-tijdGeel = 1
-tijdGroen = 5
-LDRactive = False
-while True:
-    
-    
-    if LDR.is_active:
-        
-        counter = 0
-        fietsLicht.Geel()
-        autoLicht.Geel()
-        sleep(tijdGeel)
+while True: # maak loop
+    if LDR.is_active: # als de LDR licht detecteerd
+        fietsLicht.Geel() # geel licht voor fietsers
+        autoLicht.Geel() # geel licht voor auto's
+        sleep(tijdGeel) # tijd dat het geel blijft
         fietsLicht.Groen()
         autoLicht.Rood()
         sleep(tijdGroen)
