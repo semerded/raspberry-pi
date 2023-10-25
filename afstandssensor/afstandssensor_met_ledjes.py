4baf330f5ca0714ab0b4bcf00b9584e9e6cba7f1
sem van broekhoven <sem.van.broekhoven@gmail.com>
2023-10-25 11:49:34 +0200

    final afstandssensor

--- afstandssensor/afstandssensor_met_ledjes.py
+++ afstandssensor/afstandssensor_met_ledjes.py
@@ -5,23 +5,20 @@ ledPins = [2,3,4,5,6]
 ledArray = []
 for nummer in range(5):
     ledArray.append(LED(ledPins[nummer]))
-    
 
 sensor = DistanceSensor(23,24)
 
-previousTime = time()
-activatieOp = [0.2, 0.4, 0.6, 0.8, 1]
-delay = 0.5
+activatieOp = [0.2, 0.4, 0.6, 0.8, 0.99]
 
 while True:
     currentTime = time()
+    sensorReading = sensor.distance
     
-    if currentTime - previousTime > delay:
-        for index, afstand in enumerate(activatieOp):
-            if sensor.distance > afstand:
-                ledArray[index].on()
-            else:
-                ledArray[index].off()
-        print(f"afstand tot object is {sensor.distance}m")
-        
+    for index, afstand in enumerate(activatieOp):
+        if sensorReading > afstand:
+            ledArray[index].off()
+        else:
+            ledArray[index].on()
+    print(f"afstand tot object is {sensor.distance}m")
+
 
