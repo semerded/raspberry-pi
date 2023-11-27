93bfb4320bd483247af4b1164b0c1f21588ba5b5
sem van broekhoven <sem.van.broekhoven@gmail.com>
2023-11-27 09:55:07 +0100

    final compareDHT11withDS18B20

--- temperatuursensor/compareDHT11withDS18B20.py
+++ temperatuursensor/compareDHT11withDS18B20.py
@@ -1,67 +1,49 @@
-import time
-import Adafruit_DHT
-import logging
+import time, Adafruit_DHT, logging
 from w1thermsensor import W1ThermSensor
 from gpiozero import LED
 
-
-class WarningLed:
-    def __init__(self, ledPin: int, ledBlinkSpeedMs: int = 200) -> None:
-        self.ledPin = ledPin
-        self.ledBlinkSpeed = ledBlinkSpeedMs / 2  # half time on, half time off
-        self.previousTime = 0
-        self.ledOn = False
-
-    def ledBlink(self):
-        self.currentTime = time.time
-        if self.currentTime - self.previousTime > self.ledBlinkSpeed:
-            self.ledOn = not self.ledOn
-            if self.ledOn:
-                LED(self.ledPin).on()
-            else:
-                self.ledOff()
-
-    def ledOff(self):
-        LED(self.ledPin).off()
-
-    def warningLedController(self):
-        if self.ledBlinkStatus:
-            self.ledBlink()
-        else:
-            self.ledOff()
-
-    @property.setter
-    def setWarningLed(self, status: bool):
-        self.ledBlinkStatus = status
-
-
 logging.basicConfig(filename="temperatureComparator.log",
-                    format='%(asctime)s %(message)s',
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%H:%M:%S',
                     filemode='w',
                     level=logging.INFO)
 temperatureLogger = logging.getLogger()
 
 
 DS18B20 = W1ThermSensor()
-warningLed = WarningLed()
-previousTime = 0
+warningLed = LED(26)
+previousTime = DHTfailedToReadCounter = 0
+warningLedWarningMessage = "temperatuurverschil is hoger dan 2!"
 
 while True:
-    currentTime = time.time
-    warningLed.warningLedController()
+    currentTime = time.time()
 
-    if currentTime - previousTime > 2000:  # 2 sec
-        unused_humidity, DHTtemperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4, 2)
+    if currentTime - previousTime > 2:  # 2 sec
+        if DHTfailedToReadCounter > 10:
+            temperatureLogger.error("DHT11 failed to read more than 10 times")
+            DHTfailedToReadCounter = 0
+            
+        unused_humidity, DHTtemperature = Adafruit_DHT.read(Adafruit_DHT.DHT11, 5)
         DS18B20temperature = DS18B20.get_temperature()
-
+        
+        if DHTtemperature == None or DS18B20temperature == None:
+            DHTfailedToReadCounter += 1
+            continue
+        DHTfailedToReadCounter = 0
         differenceBetweenSensors = abs(DS18B20temperature - DHTtemperature)
-        if (differenceBetweenSensors > 2):
-            warningLed.setWarningLed(True)
-        else:
-            warningLed.setWarningLed(False)
-
+        
         print("temperatuur DHT11: %s°C" % DHTtemperature)
         print("temperatuur DS18B20: %s°C" % DS18B20temperature)
         print("verschil: %s°C" % differenceBetweenSensors)
         temperatureLogger.info(f"DHT11: {DHTtemperature} | DS18B20: {DS18B20temperature} | verschil: {differenceBetweenSensors}")
+        
+        if (differenceBetweenSensors > 2):
+            warningLed.on()
+            print("-" * len(warningLedWarningMessage))
+            print(warningLedWarningMessage)
+            print("-" * len(warningLedWarningMessage))
+            temperatureLogger.warning(warningLedWarningMessage)
+        else:
+            warningLed.off()
+        print() # print een enter
         previousTime = currentTime
