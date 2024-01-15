ce40a79fd7ff95f513bf0772c5cec216f887e8af
sem van broekhoven <sem.van.broekhoven@gmail.com>
2024-01-15 09:39:56 +0100

    adc working with 2 ldrs

--- adc/adc_script.py
+++ adc/adc_script.py
@@ -1,5 +1,12 @@
-import spidev
-import time
+import spidev, time, logging
+
+LOGGING_FILE_LOCATION = "adc.log"
+
+logging.basicConfig(filename=LOGGING_FILE_LOCATION,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    filemode='a',
+                    level=logging.INFO)
+adcLogger = logging.getLogger()
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -9,9 +16,13 @@ def readSPI(channel):
     adc = spi.xfer2([1, (8 + channel) << 4,0])
     return ((adc[1] & 3) <<8) + adc[2]
     
-channelLDR = 0
+channelLDR1 = 0
+channelLDR2 = 1
 
 while True:
-    licht_intensiteit = readSPI(channelLDR)
-    print(licht_intensiteit)
+    lightSensor1 = readSPI(channelLDR1)
+    lightSensor2 = readSPI(channelLDR2)
+    averageLightValue = (lightSensor1 + lightSensor2) / 2
+    adcLogger.info(averageLightValue)
+    print("data succesfully logged")
     time.sleep(5)
