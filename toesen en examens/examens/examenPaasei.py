91ba25def3bb5804860d6c6afdb6714129da0faf
semerded <sem.van.broekhoven@gmail.com>
2024-04-19 14:31:14 +0200

    formated easter exam

--- toesen en examens/examens/examenPaasei.py
+++ toesen en examens/examens/examenPaasei.py
@@ -1,9 +1,10 @@
 from gpiozero import Button, AngularServo, PWMOutputDevice
-import Adafruit_DHT
 from threading import Thread
+import Adafruit_DHT
 
 running = True
 
+# button
 class RpiButton(Button):
     """
     remake om de `isClicked` functie toe te voegen
@@ -21,38 +22,15 @@ class RpiButton(Button):
             self.alreadyClicked = False
         return False
 
-
-class DHT11:
-    def __init__(self) -> None:
-        self.humidity: float = 0
-        self.temperature: float = 0
-
-    def getTemperatureAndHumidity(self):
-        return self._readDataFromDHT()
-    
-    def getTemperature(self):
-        return self._readDataFromDHT()[1]
-    
-    def getHumidity(self):
-        return self._readDataFromDHT()[0]
-    
-    def _readDataFromDHT(self):
-        _humidity, _temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4, 3) # lees de data van de DHT11 uit (2 staat voor hoevaak hij de data opnieuw zal proberen uit te lezen als er iets fout mocht gaan)
-        if _humidity != None:
-            self.humidity = _humidity
-        if _temperature != None:
-            self.temperature = _temperature
-        return self.humidity, self.temperature
-
-
+# mixer
 class MixerController():
-    mixerSpeedMapping = {1: 0, 2: 25, 3: 50, 4: 75, 5: 100}
+    mixerSpeedMapping: dict[int, int] = {1: 0, 2: 25, 3: 50, 4: 75, 5: 100}
     def __init__(self, motorPin: int, increaseSpeed: RpiButton, decreaseSpeed: RpiButton, confirm: RpiButton):
         self.mixMotor = PWMOutputDevice(motorPin)
-        self.increaseSpeedButton = increaseSpeed
-        self.decreaseSpeedButton = decreaseSpeed
-        self.confirmSpeedButton = confirm
-        self.currentSpeedStep = 1
+        self.increaseSpeedButton: RpiButton = increaseSpeed
+        self.decreaseSpeedButton: RpiButton = decreaseSpeed
+        self.confirmSpeedButton: RpiButton = confirm
+        self.currentSpeedStep: int = 1
         
     def mainLoop(self):
         self._readIncreaseButton()
@@ -74,12 +52,35 @@ class MixerController():
             self.mixMotor.value = self.mixerSpeedMapping[self.currentSpeedStep] / 100
             print("motorsnelheid is succesvol geupdate")
             
+# humidity sensor
+class DHT11:
+    def __init__(self) -> None:
+        self.humidity: float = 0
+        self.temperature: float = 0
+
+    def getHumidityAndTemperature(self):
+        return self._readDataFromDHT()
+    
+    def getTemperature(self):
+        return self._readDataFromDHT()[1]
+    
+    def getHumidity(self):
+        return self._readDataFromDHT()[0]
+    
+    def _readDataFromDHT(self):
+        _humidity, _temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4, 3) # lees de data van de DHT11 uit (2 staat voor hoevaak hij de data opnieuw zal proberen uit te lezen als er iets fout mocht gaan)
+        if _humidity != None:
+            self.humidity = _humidity
+        if _temperature != None:
+            self.temperature = _temperature
+        return self.humidity, self.temperature
+
             
 class HumidityController(DHT11):
     airChannelAngleMapping = {100: 170, 90: 135, 75: 90,  60: 45, 40: 10} # humidity level: angle of airchannel
     def __init__(self, servo: AngularServo) -> None:
         super().__init__()
-        self.servo = servo
+        self.servo: AngularServo = servo
 
     def startThread(self): # threaded
         Thread(target=self._threadedMainLoop).start()
@@ -98,7 +99,7 @@ class HumidityController(DHT11):
                 self.servo.angle = servoAngle - 90
                 break
                 
-
+# init classes
 decreaseSpeedButton = RpiButton(5)
 increaseSpeedButton = RpiButton(6)
 confirmSpeedButton = RpiButton(21)
@@ -107,13 +108,15 @@ mixerController = MixerController(motorPin=12, increaseSpeed=increaseSpeedButton
 
 airChannelServo = AngularServo(17)
 
-humidityController = HumidityController(airChannelServo)
+humidityController = HumidityController(servo=airChannelServo)
 humidityController.startThread() # threaded because of DHT11 measurement
+# humidity controller runs in the background
 
+# main loop
 while True:
     try:
         mixerController.mainLoop()
     except KeyboardInterrupt:
-        running = False
+        running = False # exit thread
         exit()
         
