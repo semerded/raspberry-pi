6c3dbc49fd2f230323163369965175af9d921ed0
semerded <sem.van.broekhoven@gmail.com>
2024-04-22 09:07:54 +0200

    added code to read arduino

--- arduino_communication/DHT11_motor.py
+++ arduino_communication/DHT11_motor.py
@@ -1,8 +1,27 @@
 import serial as Serial, time
+
+perviousTime: int
+currentTime: int = time.time()
+INTERVAL: int = 1000
+
+motorSpeed = 0
+
 if __name__ == '__main__':
     serial = Serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     serial.reset_input_buffer()
+    
     while True:
+        currentTime = time.time()
+        
         if serial.in_waiting > 0:
-            line = serial.readline().decode('utf-8').rstrip()
-            print(line)
+            dhtValue = serial.readline().decode('utf-8').rstrip()
+            print(dhtValue)
+            
+            try:
+                dhtValue = int(dhtValue)
+            except ValueError:
+                continue
+        
+        if currentTime - perviousTime >= INTERVAL:
+            Serial.write(b"%s\n" %motorSpeed)
+            
