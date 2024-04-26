c1a6e0e566d2e48d1824079bdfd513e13d74c6aa
semerded <sem.van.broekhoven@gmail.com>
2024-04-26 15:07:37 +0200

    testing motor range speed control

--- arduino_communication/DHT11_motor.py
+++ arduino_communication/DHT11_motor.py
@@ -1,29 +1,54 @@
-import serial as Serial, time
+import serial as Serial
+import time
 
 previousTime: int = 0
 currentTime: int = time.time()
 INTERVAL: int = 1
 
-motorSpeed = 255
+motorSpeed: int = 0
+
+MOTORSPEED_MAPPING_TEMPERATURE = (
+    (
+        20,
+
+    )
+)
+
+def getMotorSpeed(temperature, humidity):
+    if temperature > 40:
+        return 255
+    elif temperature > 35:
+        return 200
+    elif temperature > 30:
+        return 150
+    elif temperature > 20:
+        return 100
+    elif temperature > 15:
+        return 50
+    else:
+        return 0
+        
+        
+
 
 if __name__ == '__main__':
     serial = Serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     serial.reset_input_buffer()
-    
+
     while True:
         currentTime = time.time()
-        
+
         if serial.in_waiting > 0:
             dhtValue = serial.readline().decode('utf-8').rstrip()
             print(dhtValue)
-            
+
             try:
-                dhtValue = int(dhtValue)
+                temperature, humidity = dhtValue.split(";")
+                motorSpeed = getMotorSpeed(int(temperature), int(humidity))
             except ValueError:
                 continue
-        
+
         if currentTime - previousTime >= INTERVAL:
             serial.write(str(f"{motorSpeed}\n").encode('ascii'))
             # print(motorSpeed)
             previousTime = currentTime
-            
